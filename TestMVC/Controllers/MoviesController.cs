990ec3d8f2e005c87f712e6e14c72fa9c657e905
Olivia <Olivia>
2017-09-25 15:33:27 +0100

    Add project files.

---
+++ TestMVC/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shek!" }; 
+            return View(movie);
+        }
+    }
+}
