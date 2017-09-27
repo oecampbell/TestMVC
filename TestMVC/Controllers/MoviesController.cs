cd624ecfc9663a7f444498dc9ff478db06d472bd
oecampbell <ocampbell.network@gmail.com>
2017-09-27 17:00:22 +0100

    50mins inwards

--- TestMVC/Controllers/MoviesController.cs
+++ TestMVC/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TestMVC.Models;
+using TestMVC.ViewModels;
 
 namespace TestMVC.Controllers
 {
@@ -15,19 +16,27 @@ namespace TestMVC.Controllers
         {
             var movie = new Movie() { Name = "Finding Dory" };
 
-            var viewResult = new ViewResult();
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1" },
+                new Customer {Name = "Customer 2" }
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+
+
+
+
+
+
         }
-        [Route("movies/released/{year}/{month:regex(\\d{4:range(1,12)})}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-
-        }
-
-        
-    }   
- 
+    } 
 
 }
